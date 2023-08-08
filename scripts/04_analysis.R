b16258fef37abbd373b41366816a6e7bae39d07d
Alexander Ohnmacht <ohnmachtalexander@gmail.com>
2023-08-08 10:41:25 +0200

    analysis updates

--- scripts/04_analysis.R
+++ scripts/04_analysis.R
@@ -13,7 +13,7 @@ files <- list.files(path = paste0(path_calcs,"metadata/summaries"),full.names = 
 
 
 
-names <- c("drug","drugname","TCGA","pvalue","fdr","effectsize","delta","cor_emt","cor_mut","cor_emt_sd","cor_mut_sd","method","resp_type","score","fraction_extrapolated","kept_extrapolated")
+names <- c("drug","drugname","TCGA","pvalue","fdr","effectsize","effectsize_mean","lower","upper","inference","delta","cor_emt","cor_mut","cor_emt_sd","cor_mut_sd","method","resp_type","score","fraction_extrapolated","kept_extrapolated")
 ll <- list()
 for( i in 1:length(files) ){ ## c(168,429)
   print(i)
@@ -27,7 +27,11 @@ for( i in 1:length(files) ){ ## c(168,429)
       dplyr::mutate(fdr = p.adjust(pvalue, method = "BH")) %>%
       ungroup %>%
       dplyr::mutate(fdr = ifelse(kept_extrapolated, fdr, NA)) %>%
+      dplyr::mutate(inference = NA) %>%
+      dplyr::mutate(effectsize_mean = NA) %>%
       dplyr::mutate(effectsize = NA) %>%
+      dplyr::mutate(upper = NA) %>%
+      dplyr::mutate(lower = NA) %>%
       dplyr::mutate(method = "eln") %>%
       dplyr::mutate(resp_type = "ic50")
   }
@@ -39,7 +43,11 @@ for( i in 1:length(files) ){ ## c(168,429)
       dplyr::mutate(fdr = p.adjust(pvalue, method = "BH")) %>%
       #ungroup %>%
       #dplyr::mutate(fdr = ifelse(kept_extrapolated, fdr, NA)) %>%
+      dplyr::mutate(inference = NA) %>%
+      dplyr::mutate(effectsize_mean = NA) %>%
       dplyr::mutate(effectsize = NA) %>%
+      dplyr::mutate(upper = NA) %>%
+      dplyr::mutate(lower = NA) %>%
       dplyr::mutate(method = "eln") %>%
       dplyr::mutate(resp_type = "auc")
   }
@@ -47,7 +55,11 @@ for( i in 1:length(files) ){ ## c(168,429)
   if(grepl("grf",files[i], fixed = TRUE)&(!grepl("auc",files[i], fixed = TRUE))){
     tmp$effect_p <- lapply(tmp$effect, function(y) unlist(lapply(y, function(x) unique(x)[2])))
     tmp$pvalue <- unlist(lapply(tmp$effect_p, function(x) as.numeric(mean(x)))) # harmonicmeanp::hmp.stat
-    tmp$effectsize <- unlist(lapply(tmp$effect, function(y) mean(unlist(lapply(y, function(x) unique(x)[5])))))
+    tmp$inference <- tmp$effect#(lapply(tmp$effect, function(y) (unlist(lapply(y, function(x) unique(x))))))
+    tmp$effectsize_mean <- unlist(lapply(tmp$effect, function(y) mean(unlist(lapply(y, function(x) unique(x)[3])))))
+    tmp$effectsize <- (lapply(tmp$effect, function(y) (unlist(lapply(y, function(x) unique(x)[3])))))
+    tmp$lower <- (lapply(tmp$effect, function(y) (unlist(lapply(y, function(x) unique(x)[4])))))
+    tmp$upper <- (lapply(tmp$effect, function(y) (unlist(lapply(y, function(x) unique(x)[5])))))
     tmp <- tmp %>% 
       dplyr::mutate(kept_extrapolated = fraction_extrapolated < extrapolated_threshold) %>%
       group_by(kept_extrapolated) %>%
@@ -62,7 +74,11 @@ for( i in 1:length(files) ){ ## c(168,429)
   if(grepl("grf",files[i], fixed = TRUE)&(grepl("auc",files[i], fixed = TRUE))){
     tmp$effect_p <- lapply(tmp$effect, function(y) unlist(lapply(y, function(x) unique(x)[2])))
     tmp$pvalue <- unlist(lapply(tmp$effect_p, function(x) as.numeric(mean(x)))) # harmonicmeanp::hmp.stat
-    tmp$effectsize <- unlist(lapply(tmp$effect, function(y) mean(unlist(lapply(y, function(x) unique(x)[5])))))
+    tmp$inference <- tmp$effect#(lapply(tmp$effect, function(y) (unlist(lapply(y, function(x) unique(x))))))
+    tmp$effectsize_mean <- unlist(lapply(tmp$effect, function(y) mean(unlist(lapply(y, function(x) unique(x)[3])))))
+    tmp$effectsize <- (lapply(tmp$effect, function(y) (unlist(lapply(y, function(x) unique(x)[3])))))
+    tmp$lower <- (lapply(tmp$effect, function(y) (unlist(lapply(y, function(x) unique(x)[4])))))
+    tmp$upper <- (lapply(tmp$effect, function(y) (unlist(lapply(y, function(x) unique(x)[5])))))
     tmp <- tmp %>% 
       dplyr::mutate(kept_extrapolated = fraction_extrapolated < extrapolated_threshold) %>%
       #group_by(kept_extrapolated) %>%
@@ -92,15 +108,79 @@ for( i in 1:length(files) ){ ## c(168,429)
   ll[[i]] <- tmp
 }
 ll_df <- do.call(rbind, ll)
-
-saveRDS(object = ll_df, paste0(path_calcs,"metadata/summaries/PERFORMANCES_v1.rds"))
-saveRDS(object = ll_df, paste0(path,"metadata/summaries/PERFORMANCES_v1.rds"))
+ll_df$index <- 1:nrow(ll_df)
 
 
-
-
+saveRDS(object = ll_df, paste0(path_calcs,"metadata/summaries/PERFORMANCES_v2.rds"))
+saveRDS(object = ll_df, paste0(path,"metadata/summaries/PERFORMANCES_v2.rds"))
 
 
 
 
+
+
+
+
+
+if(F){ # test combine CI methods....
+test <- ll_df[(ll_df$drug == "1559-GDSC2")&(ll_df$TCGA == "SKCM")&(ll_df$method == "grf")&(ll_df$resp_type == "ic50")&(ll_df$score == "mak"),]
+u <- test$upper[[1]]
+l <- test$lower[[1]]
+e <- test$effectsize[[1]]
+
+#### way too small variance
+Variance <- ((u - l) / (2 * qnorm(0.975)))^2
+InvVariance <- 1 / Variance
+weighted_mean <- sum(e * InvVariance) / sum(InvVariance)
+overall_variance <- 1 / sum(InvVariance)
+
+z_score <- qnorm(1 - (1 - 0.95) / 2)
+margin_of_error <- z_score * sqrt(overall_variance)
+
+l_o <- weighted_mean - margin_of_error
+u_o <- weighted_mean + margin_of_error
+e_o <- weighted_mean
+###########################
+
+#### percentile method
+overall_lower <- quantile(l, probs = 0.025)
+overall_upper <- quantile(u, probs = 0.975)
+
+l_o <- overall_lower
+u_o <- overall_upper
+e_o <- mean(e)
+###########################
+
+#### percentile method
+# Combine confidence intervals using weighted average
+weights <- rep(40, 25)  # Vector of sample sizes
+combined_lower <- sum(l * weights) / sum(weights)
+combined_upper <- sum(u * weights) / sum(weights)
+
+# Adjust confidence interval for cross-validation
+num_folds <- length(l/5)
+adjustment_factor <- sqrt(num_folds)
+adjusted_interval <- c((combined_lower + combined_upper) / 2,
+                       (combined_upper - combined_lower) / (2 * adjustment_factor))
+
+l_o <- adjusted_interval[1] - adjusted_interval[2]
+u_o <- adjusted_interval[1] + adjusted_interval[2]
+e_o <- mean(e)
+###########################
+
+
+df <- data.frame(label = c("overall",1:25), mean = c(e_o,e), lower = c(l_o,l), upper = c(u_o,u))
+
+# reverses the factor level ordering for labels after coord_flip()
+df$label <- factor(df$label, levels=rev(df$label))
+
+library(ggplot2)
+fp <- ggplot(data=df, aes(x=label, y=mean, ymin=lower, ymax=upper)) +
+  geom_pointrange() + 
+  geom_hline(yintercept=0, lty=2) +  # add a dotted line at x=1 after flip
+  coord_flip() +  # flip coordinates (puts labels on y axis)
+  xlab("Label") + ylab("Mean (95% CI)") +
+  theme_bw()  # use a white background
+print(fp)
+}
 
--- scripts/07_analysis.R
+++ scripts/07_analysis.R
@@ -17,10 +17,186 @@ df <- left_join(df, drug_meta, by = "drugname")
 
 
 
-# 1. density plot of two methods
-ggplot(data = df)+
-  geom_density(aes(x = cor_emt, color = method))+
-  theme_minimal()
+# figure S1 (performances (fdr) dependent on all 'drugname'&'TCGA'&'resp_type','emtscore')
+# only showing cancer types and drugs with at least one FDR<0.2
+# gaps are not enough samples and extrapolated IC50 (Methods)
+df_4 <- df %>%
+  #dplyr::filter(resp_type == "ic50") %>%
+  #dplyr::filter(!is.na(fdr)) %>%
+  #dplyr::filter(score == "mak") %>%
+  dplyr::filter(method == "eln") %>%
+  dplyr::select(c(fdr, pvalue, delta, score,drug, drugname, TCGA, resp_type)) %>%
+  #dplyr::filter(drugname %in% unique(df$drugname)[1:100]) %>%
+  mutate(id = paste0(score," (",toupper(resp_type),")")) %>%
+  mutate(drugname2 = paste0(drugname," (",sub(".*-","",drug),")"))
+df_4 <- df_4 %>%
+  dplyr::filter(TCGA %in% unique((df_4 %>% group_by(TCGA) %>% dplyr::filter(fdr < 0.2))$TCGA)) %>% # filter for TCGA with at least one < 0.2
+  dplyr::filter(drug %in% unique((df_4 %>% group_by(drug) %>% dplyr::filter(fdr < 0.2))$drug)) %>% # filter for drugname with at least one < 0.2 
+  mutate(txt = ifelse(fdr < 0.2,"-","")) %>%
+  mutate(txt = ifelse(is.na(txt), "", txt))
+h <- ggplot(df_4, aes(x = id, y = drugname2, fill = -log10(pvalue))) +
+  geom_tile() +
+  facet_grid(. ~ TCGA, scales = "free", space = "free")+
+  scale_fill_continuous(na.value = 'grey88')+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))+
+  geom_text(aes(label=txt), color = "white"); h
+
+
+
+# Fig. 2 volcano for all cancer types and drugs (BRCA, SKCM, GBM, LUAD, SCLC, COREAD)
+fdr_threshold <- 0.2
+df_3 <- df %>%
+  #dplyr::filter(resp_type == "ic50") %>%
+  dplyr::filter(!is.na(fdr)) %>%
+#dplyr::filter(score == "mak") %>%
+  dplyr::filter(TCGA != "PANCAN") %>%
+  dplyr::filter(TCGA %in% as.character(unlist(unique(na.omit(df[(df$method == "eln")&(df$fdr < fdr_threshold),"TCGA"]))))) 
+df_3 <- df_3 %>%
+  dplyr::select(c(method, drugname, drug,TCGA,score,pvalue,fdr,resp_type,cor_emt,effectsize,delta)) %>%
+  tidyr::pivot_wider(names_from = method, values_from = c(pvalue,fdr,cor_emt,effectsize)) %>%
+  #dplyr::filter(drug == "1559-GDSC2") %>%
+  mutate(id = paste0(drugname,"-",score,"-",resp_type)) %>%
+  mutate(strat = paste0(drug,"-",score,"-",resp_type,"-",TCGA)) %>% 
+  mutate(filt = paste0(drugname,"-",TCGA))
+df_3 <- df_3 %>% group_by(strat) %>% summarize(effectsize_grf = -na.omit(effectsize_grf), 
+                                                  TCGA = TCGA, 
+                                                  score = score, 
+                                                  drugname = drugname,
+                                                  resp_type = resp_type,
+                                                  pvalue_eln = na.omit(pvalue_eln),
+                                                  pvalue_grf = na.omit(pvalue_grf),
+                                                  delta = na.omit(delta),
+                                                  fdr_eln = na.omit(fdr_eln),
+                                                  filt = filt,
+                                                  id = id) %>% 
+  distinct %>% 
+  ungroup()
+#df_3$effectsize_grf[df_3$score == "mak"] <- -df_3$effectsize_grf[df_3$score == "mak"] ### FIX THIS !
+x <- "delta"
+y <- "pvalue_eln"
+h0 <- max((df_3[,c(y,"fdr_eln")] %>% arrange(fdr_eln) %>% filter(fdr_eln < fdr_threshold))[,y])
+#h0 <- sort(unlist(na.omit(df_3[,"pvalue_eln"])))[max(which(sort(unlist(na.omit(df_3[,"fdr_eln"])))<=fdr_threshold))]
+p <- ggplot()+
+  geom_point(data = df_3, aes(x = !!sym(x), y = -log10(!!sym(y)), color = TCGA), alpha = 0.2)+
+  geom_point(data = df_3[(df_3$fdr_eln < fdr_threshold),], aes(x = !!sym(x), y = -log10(!!sym(y)), color = TCGA), alpha = 0.8)+
+  geom_text_repel(data = df_3[(df_3$fdr_eln < fdr_threshold)&(df_3$filt %in% names(which(table(df_3[df_3$fdr_eln < 0.2,"filt"] ) >=3))),],
+                  aes(x = !!sym(x), y = -log10(!!sym(y)), label = id), color = "black", size = 4, show_guide = F, min.segment.length = 0.02)+
+  theme_minimal()+
+  xlab("delta Pearson correlation")+
+  ylab("unadjusted -log10(p)")+
+  geom_hline(yintercept=-log10(h0), linetype='dotted', color = "grey66", size = 0.9)+
+  annotate("text", x = min(df_3[,x]%>%na.omit), y = -log10(h0), label = paste0("FDR<",as.character(round(fdr_threshold*100)),"%"), hjust = 0, vjust = 0); p
+
+
+
+# supplement volcano for cancer types and drugs (PANCAN)
+fdr_threshold <- 0.2
+df_3 <- df %>%
+  #dplyr::filter(resp_type == "ic50") %>%
+  dplyr::filter(!is.na(fdr)) %>%
+  #dplyr::filter(score == "mak") %>%
+  dplyr::filter(TCGA %in% "PANCAN") 
+df_3 <- df_3 %>%
+  dplyr::select(c(method, drugname, drug,TCGA,score,pvalue,fdr,resp_type,cor_emt,effectsize,delta)) %>%
+  tidyr::pivot_wider(names_from = method, values_from = c(pvalue,fdr,cor_emt,effectsize)) %>%
+  #dplyr::filter(drug == "1559-GDSC2") %>%
+  mutate(id = paste0(drugname,"-",score,"-",resp_type)) %>%
+  mutate(strat = paste0(drug,"-",score,"-",resp_type,"-",TCGA)) %>% 
+  mutate(filt = paste0(drugname,"-",TCGA))
+df_3 <- df_3 %>% group_by(strat) %>% summarize(effectsize_grf = -na.omit(effectsize_grf), 
+                                               TCGA = TCGA, 
+                                               score = score, 
+                                               drugname = drugname,
+                                               resp_type = resp_type,
+                                               pvalue_eln = na.omit(pvalue_eln),
+                                               pvalue_grf = na.omit(pvalue_grf),
+                                               delta = na.omit(delta),
+                                               fdr_eln = na.omit(fdr_eln),
+                                               filt = filt,
+                                               id = id) %>% 
+  distinct %>% 
+  ungroup()
+#df_3$effectsize_grf[df_3$score == "mak"] <- -df_3$effectsize_grf[df_3$score == "mak"] ### FIX THIS !
+x <- "delta"
+y <- "pvalue_eln"
+h0 <- max((df_3[,c(y,"fdr_eln")] %>% arrange(fdr_eln) %>% filter(fdr_eln < fdr_threshold))[,y])
+#h0 <- sort(unlist(na.omit(df_3[,"pvalue_eln"])))[max(which(sort(unlist(na.omit(df_3[,"fdr_eln"])))<=fdr_threshold))]
+p <- ggplot()+
+  geom_point(data = df_3, aes(x = !!sym(x), y = -log10(!!sym(y)), color = TCGA), alpha = 0.2)+
+  geom_point(data = df_3[(df_3$fdr_eln < fdr_threshold),], aes(x = !!sym(x), y = -log10(!!sym(y)), color = TCGA), alpha = 0.8)+
+  geom_text_repel(data = df_3[(df_3$fdr_eln < fdr_threshold)&(df_3$filt %in% names(which(table(df_3[df_3$fdr_eln < 0.2,"filt"] ) >=3))),],
+                  aes(x = !!sym(x), y = -log10(!!sym(y)), label = id), color = "black", size = 4, show_guide = F, min.segment.length = 0.02)+
+  theme_minimal()+
+  xlab("delta Pearson correlation")+
+  ylab("unadjusted -log10(p)")+
+  geom_hline(yintercept=-log10(h0), linetype='dotted', color = "grey66", size = 0.9)+
+  annotate("text", x = min(df_3[,x]%>%na.omit), y = -log10(h0), label = paste0("FDR<",as.character(round(fdr_threshold*100)),"%"), hjust = 0, vjust = 0); p
+
+
+
+
+# Figure 1 volcano for cancer types and drugs (SKCM)
+df_3 <- df %>%
+  #dplyr::filter(resp_type == "ic50") %>%
+  dplyr::filter(!is.na(fdr)) %>%
+  #dplyr::filter(score == "mak") %>%
+  dplyr::filter(TCGA == "SKCM") 
+df_3 <- df_3 %>%
+  dplyr::select(c(method, drugname, drug,TCGA,score,pvalue,fdr,resp_type,cor_emt,effectsize,delta)) %>%
+  tidyr::pivot_wider(names_from = method, values_from = c(pvalue,fdr,cor_emt,effectsize)) %>%
+  #dplyr::filter(drug == "1559-GDSC2") %>%
+  mutate(id = paste0(drugname,"-",score,"-",resp_type)) %>%
+  mutate(strat = paste0(drug,"-",score,"-",resp_type,"-",TCGA)) %>% 
+  mutate(filt = paste0(drugname,"-",TCGA))
+df_3 <- df_3 %>% group_by(c(strat)) %>% summarize(effectsize_grf = -na.omit(effectsize_grf), 
+                                               TCGA = TCGA, 
+                                               score = score, 
+                                               drugname = drugname,
+                                               resp_type = resp_type,
+                                               pvalue_eln = na.omit(pvalue_eln),
+                                               pvalue_grf = na.omit(pvalue_grf),
+                                               delta = na.omit(delta),
+                                               fdr_eln = na.omit(fdr_eln),
+                                               filt = filt,
+                                               id = id) %>% 
+  distinct %>% 
+  ungroup()
+#df_3$effectsize_grf[df_3$score == "mak"] <- -df_3$effectsize_grf[df_3$score == "mak"] ### FIX THIS !
+fdr_threshold <- 0.2
+x <- "delta"
+y <- "pvalue_eln"
+h0 <- max((df_3[,c(y,"fdr_eln")] %>% arrange(fdr_eln) %>% filter(fdr_eln < fdr_threshold))[,y])
+#h0 <- sort(unlist(na.omit(df_3[,"pvalue_eln"])))[max(which(sort(unlist(na.omit(df_3[,"fdr_eln"])))<=fdr_threshold))]
+p <- ggplot()+
+  geom_point(data = df_3, aes(x = !!sym(x), y = -log10(!!sym(y)), color = TCGA), alpha = 0.2)+
+  geom_point(data = df_3[(df_3$fdr_eln < fdr_threshold),], aes(x = !!sym(x), y = -log10(!!sym(y)), color = TCGA), alpha = 0.8)+
+  geom_text_repel(data = df_3[(df_3$fdr_eln < fdr_threshold)&(df_3$filt %in% names(which(table(df_3[df_3$fdr_eln < 0.2,"filt"] ) >=3))),],
+                  aes(x = !!sym(x), y = -log10(!!sym(y)), label = id), color = "black", size = 4, show_guide = F, min.segment.length = 0.02)+
+  theme_minimal()+
+  xlab("delta Pearson correlation")+
+  ylab("unadjusted -log10(p)")+
+  geom_hline(yintercept=-log10(h0), linetype='dotted', color = "grey66", size = 0.9)+
+  annotate("text", x = min(df_3[,x]%>%na.omit), y = -log10(h0), label = paste0("FDR<",as.character(round(fdr_threshold*100)),"%"), hjust = 0, vjust = 0); p
+
+
+
+
+
+
+# EMT correlations
+EMTscores_mak <- suppressMessages(read_csv(paste0(path,"metadata/EMTscores","",".csv"))) #%>% mutate(EMT_score = -EMT_score) #%>% mutate(method = "mak")
+EMTscores_gsva <- suppressMessages(read_csv(paste0(path,"metadata/EMTscores","_gsva",".csv"))) #%>% mutate(method = "gsva")
+EMTscores_tan <- suppressMessages(read_csv(paste0(path,"metadata/EMTscores","_tan_088",".csv"))) #%>% mutate(method = "tan")
+EMTscores_secrier <- suppressMessages(read_csv(paste0(path,"metadata/EMTscores","_secrier_065",".csv"))) #%>% mutate(method = "secrier")
+emt <- full_join(full_join(full_join(EMTscores_mak, EMTscores_gsva, by = "COSMIC ID"), EMTscores_tan, by = "COSMIC ID"), EMTscores_secrier, by = "COSMIC ID")
+emtp <- select_if(emt, is.numeric)
+colnames(emtp) <- c("cosmic","Mak","GSVA","Tan","Secrier")
+cor(emtp[emt$`TCGA Desc.x` == "SKCM",-1], use = "complete.obs")
+
+
+
+
+
 
 # 2. volcano of all stuff
 df_2 <- df %>% 
@@ -40,83 +216,14 @@ ggplot(data = df_2)+
   geom_abline(intercept = 0, linetype = "dashed")+
   theme(aspect.ratio=1)
 
-ggplot(data = df_2)+
-  geom_point(aes(x = cor_emt_eln, y = cor_emt_grf), color = "black", alpha = 0.05)+
-  geom_point(data = df_2[(df_2$fdr_eln < 0.2),], aes(x = cor_emt_eln, y = cor_emt_grf, color = TCGA), alpha = 0.8)+
-  geom_text_repel(data = df_2[(df_2$fdr_eln < 0.2),],
-                  aes(x = cor_emt_eln, y = cor_emt_grf, label = drugname), color = "black", size = 4, show_guide = F, min.segment.length = 0.02)+
-  theme_minimal()+
-  xlab("Pearson's r standard")+
-  ylab("Pearson's r causal")+
-  geom_abline(intercept = 0, linetype = "dashed")+
-  theme(aspect.ratio=1)
+
+# 1. density plot of two methods
+ggplot(data = df)+
+  geom_density(aes(x = cor_emt, color = method))+
+  theme_minimal()
 
 
-# 3. barplot for one drug and cancer type (SKCM)
-df_3 <- df %>%
-  dplyr::filter(resp_type == "ic50") %>%
-  dplyr::filter(!is.na(fdr)) %>%
-  #dplyr::filter(score == "mak") %>%
-  dplyr::filter(TCGA == "SKCM") %>%
-  dplyr::select(c(method, drugname, drug,TCGA,score,pvalue,fdr,resp_type,cor_emt,effectsize,delta)) %>%
-  tidyr::pivot_wider(names_from = method, values_from = c(pvalue,fdr,cor_emt,effectsize)) %>%
-  #dplyr::filter(drug == "1559-GDSC2") %>%
-  mutate(id = paste0(drugname,"-",score,"-",resp_type))
-df_3$effectsize_grf[df_3$score == "mak"] <- -df_3$effectsize_grf[df_3$score == "mak"]
-fdr_threshold <- 0.2
-x <- "effectsize"
-h0 <- sort(unlist(na.omit(df_3[,"pvalue_eln"])))[max(which(sort(unlist(na.omit(df_3[,"fdr_eln"])))<fdr_threshold))]
-p <- ggplot()+
-  geom_point(data = df_3, aes(x = !!sym(x), y = -log10(pvalue_eln), color = score), alpha = 0.2)+
-  geom_point(data = df_3[(df_3$delta > 0.3),], aes(x = !!sym(x), y = -log10(pvalue_eln), color = score), alpha = 0.8)+
-  geom_text_repel(data = df_3[(df_3$delta > 0.3),],
-                  aes(x = !!sym(x), y = -log10(pvalue_eln), label = id), color = "black", size = 4, show_guide = F, min.segment.length = 0.02)+
-  theme_minimal()+
-  xlab("treatment effect estimate")+
-  ylab("unadjusted -log10(p)")+
-  geom_hline(yintercept=-log10(h0), linetype='dotted', color = "grey66", size = 0.9)+
-  annotate("text", x = min(df_3[,x]%>%na.omit), y = -log10(h0), label = paste0("FDR<",as.character(round(fdr_threshold*100)),"%"), hjust = 0, vjust = 0); p
-
-
-
-# figure S1 (performances (fdr, delta) of 'score' dependent on 'drugname'&'TCGA')
-df_4 <- df %>%
-  #dplyr::filter(resp_type == "ic50") %>%
-  #dplyr::filter(!is.na(fdr)) %>%
-  #dplyr::filter(score == "mak") %>%
-  dplyr::filter(method == "eln") %>%
-  dplyr::select(c(fdr, pvalue, delta, score,drug, drugname, TCGA, resp_type)) %>%
-  #dplyr::filter(drugname %in% unique(df$drugname)[1:100]) %>%
-  mutate(id = paste0(score," (",toupper(resp_type),")")) %>%
-  mutate(drugname2 = paste0(drugname," (",sub(".*-","",drug),")")) %>%
-  dplyr::filter(TCGA %in% unique((df_4 %>% group_by(TCGA) %>% dplyr::filter(fdr < 0.2))$TCGA)) %>% # filter for TCGA with at least one < 0.2
-  dplyr::filter(drugname %in% unique((df_4 %>% group_by(drugname) %>% dplyr::filter(fdr < 0.2))$drugname)) %>% # filter for drugname with at least one < 0.2 
-  mutate(txt = ifelse(fdr < 0.2,"-","")) %>%
-  mutate(txt = ifelse(is.na(txt), "", txt))
-  
-
-h <- ggplot(df_4, aes(x = id, y = drugname2, fill = -log10(pvalue))) +
-  geom_tile() +
-  facet_grid(. ~ TCGA, scales = "free", space = "free")+
-  scale_fill_continuous(na.value = 'grey88')+
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))+
-  geom_text(aes(label=txt), color = "white"); h
-
-
-# EMT correlations
-EMTscores_mak <- suppressMessages(read_csv(paste0(path,"metadata/EMTscores","",".csv"))) #%>% mutate(EMT_score = -EMT_score) #%>% mutate(method = "mak")
-EMTscores_gsva <- suppressMessages(read_csv(paste0(path,"metadata/EMTscores","_gsva",".csv"))) #%>% mutate(method = "gsva")
-EMTscores_tan <- suppressMessages(read_csv(paste0(path,"metadata/EMTscores","_tan_088",".csv"))) #%>% mutate(method = "tan")
-EMTscores_secrier <- suppressMessages(read_csv(paste0(path,"metadata/EMTscores","_secrier_065",".csv"))) #%>% mutate(method = "secrier")
-emt <- full_join(full_join(full_join(EMTscores_mak, EMTscores_gsva, by = "COSMIC ID"), EMTscores_tan, by = "COSMIC ID"), EMTscores_secrier, by = "COSMIC ID")
-emtp <- select_if(emt, is.numeric)
-colnames(emtp) <- c("cosmic","Mak","GSVA","Tan","Secrier")
-cor(emtp[emt$`TCGA Desc.x` == "SKCM",-1], use = "complete.obs")
-
-
-
-
-
+# failing enrichments
 en_df <- list();for(i in 1:nrow(hm_fig1_en)){
   en <- enrichment(tab = table(drug_targets[drug_targets %in% names(which(table(drug_targets)>=1))], 
                                hm_fig1_en[i,drug_targets %in% names(which(table(drug_targets)>=1))]%>%unlist), 
