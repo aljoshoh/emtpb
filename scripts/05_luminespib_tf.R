b23aaa03cf30a4b06a67ca2a7b545e18c403fb8e
Alexander Ohnmacht <ohnmachtalexander@gmail.com>
2023-09-08 13:06:04 +0200

    update paper figs

--- scripts/05_luminespib_tf.R
+++ scripts/05_luminespib_tf.R
@@ -73,7 +73,10 @@ for(j in 1:nrow(label_data)){
   limma_luminespib_neg <- my_results$gene[(my_results$adj.P.Val < 0.1)&(my_results$logFC < 0)]
   
   if((ct == "SKCM") & (dr == "1559-GDSC2") & (resp_type == "auc")){
-    limma_luminespib <- limma_luminespib_pos
+    limma_luminespib_auc <- limma_luminespib_pos
+  }
+  if((ct == "SKCM") & (dr == "1559-GDSC2") & (resp_type == "ic50")){
+    limma_luminespib_ic50 <- limma_luminespib_pos
   }
   
   # enrichment
@@ -85,64 +88,72 @@ for(j in 1:nrow(label_data)){
   enriched$cancertype <- ct
   enriched$drug <- dr
   enriched$drugname <-  label_data$drugname[j]
+  enriched$resp_type <-  label_data$resp_type[j]
   
   lenriched[[j]] <- enriched
 }
 enr <- rbindlist(lenriched)
-saveRDS(enr, file = "emtpb/metadata/paper/tf_target_enrichments.rds")
-saveRDS(limma_luminespib, file = "emtpb/metadata/paper/tf_diff_genes_luminespib.rds")
-
-
-# genes MITF targets
-#strsplit(enr_luminespib$Genes[enr_luminespib$Term == "MITF 21258399 ChIP-Seq MELANOMA Human"][1],";")
-
-colors_genes <- c(limma_luminespib_neg, limma_luminespib_pos)
-names(colors_genes) = colors_genes
-colors_genes[rep(T, length(colors_genes))] <- "black"
-colors_genes[names(colors_genes) %in% strsplit(enriched$Genes[enriched$Term == "MITF 21258399 ChIP-Seq MELANOMA Human" & (enriched$sign == "positive")],";")[[1]]] <- "deepskyblue"
-colors_genes[names(colors_genes) %in% strsplit(enriched$Genes[enriched$Term == "FIBROBLAST" & (enriched$sign == "negative")],";")[[1]]] <- "darkorange"
-colors_genes[names(colors_genes) %in% c("CDH1","CDH2")] <- "deepskyblue4"
-saveRDS(colors_genes, file = "metadata/paper/colors_genes_enrichr_luminespib_melanoma.rds")
+saveRDS(enr, file = paste0(path,"metadata/paper/tf_target_enrichments_v2.rds"))
+saveRDS(limma_luminespib_auc, file = paste0(path,"metadata/paper/tf_diff_genes_luminespib_auc.rds"))
+saveRDS(limma_luminespib_ic50, file = paste0(path,"metadata/paper/tf_diff_genes_luminespib_ic50.rds"))
 
 
 
 
 
-# luad rock inhibitor
-gex_gdsc_t <- read_csv(paste0(path,"metadata/matrix_exp.csv")) %>% dplyr::filter(`COSMIC ID` %in% (EMTscores$`COSMIC ID`[EMTscores$`TCGA Desc` == "LUAD"]))
-luad_emt <- EMTscores[EMTscores$`TCGA Desc` == "LUAD",]
-luad_emt <- left_join(luad_emt, resp[,c("COSMIC ID","1192-GDSC1"),drop = F])
-tmp <- left_join(luad_emt, gex_gdsc_t, by = c("COSMIC ID" = "COSMIC ID"))
-tmp <- na.omit(tmp)
-exp_luad <- tmp[,-c(1:5)]
-rock_resp_luad <- tmp$`1192-GDSC1`
-
-my_design <- model.matrix(~rock_resp_luad)
-library(limma)
-my_fit <- lmFit(exp_luad%>%as.matrix%>%t, my_design)
-my_ebayes <- eBayes(my_fit)
-my_results <- topTable(my_ebayes, n=nrow(my_ebayes))%>%rownames_to_column("gene")
-
-
-limma_rock_pos <- my_results$gene[(my_results$adj.P.Val < 0.1)&(my_results$logFC > 0)]
-limma_rock_neg <- my_results$gene[(my_results$adj.P.Val < 0.1)&(my_results$logFC < 0)]
-
-library(enrichR)
-terms <- c("ARCHS4_Tissues","ChEA_2022","RNAseq_Automatic_GEO_Signatures_Human_Up","RNAseq_Automatic_GEO_Signatures_Human_Down","LINCS_L1000_CRISPR_KO_Consensus_Sigs")
-enriched_pos <- enrichr(limma_rock_pos, terms); enriched_pos <- do.call(rbind, enriched_pos); enriched_pos$sign <- "positive"
-enriched_neg <- enrichr(limma_rock_neg, terms); enriched_neg <- do.call(rbind, enriched_neg); enriched_neg$sign <- "negative"
-enriched <- rbind(enriched_pos, enriched_neg)
-
-
-
-
-
-# exploration of gene lists in LUAD (rock inhibitor)
+# DEPRECATED
 if(F){
-  dbs <- listEnrichrDbs()
-  terms <- c("RNAseq_Automatic_GEO_Signatures_Human_Up","RNAseq_Automatic_GEO_Signatures_Human_Down","LINCS_L1000_CRISPR_KO_Consensus_Sigs")
+  # genes MITF targets
+  #strsplit(enr_luminespib$Genes[enr_luminespib$Term == "MITF 21258399 ChIP-Seq MELANOMA Human"][1],";")
+  
+  colors_genes <- c(limma_luminespib_neg, limma_luminespib_pos)
+  names(colors_genes) = colors_genes
+  colors_genes[rep(T, length(colors_genes))] <- "black"
+  colors_genes[names(colors_genes) %in% strsplit(enriched$Genes[enriched$Term == "MITF 21258399 ChIP-Seq MELANOMA Human" & (enriched$sign == "positive")],";")[[1]]] <- "deepskyblue"
+  colors_genes[names(colors_genes) %in% strsplit(enriched$Genes[enriched$Term == "FIBROBLAST" & (enriched$sign == "negative")],";")[[1]]] <- "darkorange"
+  colors_genes[names(colors_genes) %in% c("CDH1","CDH2")] <- "deepskyblue4"
+  saveRDS(colors_genes, file = "metadata/paper/colors_genes_enrichr_luminespib_melanoma.rds")
+  
+  
+  
+  
+  
+  # luad rock inhibitor
+  gex_gdsc_t <- read_csv(paste0(path,"metadata/matrix_exp.csv")) %>% dplyr::filter(`COSMIC ID` %in% (EMTscores$`COSMIC ID`[EMTscores$`TCGA Desc` == "LUAD"]))
+  luad_emt <- EMTscores[EMTscores$`TCGA Desc` == "LUAD",]
+  luad_emt <- left_join(luad_emt, resp[,c("COSMIC ID","1192-GDSC1"),drop = F])
+  tmp <- left_join(luad_emt, gex_gdsc_t, by = c("COSMIC ID" = "COSMIC ID"))
+  tmp <- na.omit(tmp)
+  exp_luad <- tmp[,-c(1:5)]
+  rock_resp_luad <- tmp$`1192-GDSC1`
+  
+  my_design <- model.matrix(~rock_resp_luad)
+  library(limma)
+  my_fit <- lmFit(exp_luad%>%as.matrix%>%t, my_design)
+  my_ebayes <- eBayes(my_fit)
+  my_results <- topTable(my_ebayes, n=nrow(my_ebayes))%>%rownames_to_column("gene")
+  
+  
+  limma_rock_pos <- my_results$gene[(my_results$adj.P.Val < 0.1)&(my_results$logFC > 0)]
+  limma_rock_neg <- my_results$gene[(my_results$adj.P.Val < 0.1)&(my_results$logFC < 0)]
+  
+  library(enrichR)
+  terms <- c("ARCHS4_Tissues","ChEA_2022","RNAseq_Automatic_GEO_Signatures_Human_Up","RNAseq_Automatic_GEO_Signatures_Human_Down","LINCS_L1000_CRISPR_KO_Consensus_Sigs")
   enriched_pos <- enrichr(limma_rock_pos, terms); enriched_pos <- do.call(rbind, enriched_pos); enriched_pos$sign <- "positive"
   enriched_neg <- enrichr(limma_rock_neg, terms); enriched_neg <- do.call(rbind, enriched_neg); enriched_neg$sign <- "negative"
   enriched <- rbind(enriched_pos, enriched_neg)
+  
+  
+  
+  
+  
+  # exploration of gene lists in LUAD (rock inhibitor)
+  if(F){
+    dbs <- listEnrichrDbs()
+    terms <- c("RNAseq_Automatic_GEO_Signatures_Human_Up","RNAseq_Automatic_GEO_Signatures_Human_Down","LINCS_L1000_CRISPR_KO_Consensus_Sigs")
+    enriched_pos <- enrichr(limma_rock_pos, terms); enriched_pos <- do.call(rbind, enriched_pos); enriched_pos$sign <- "positive"
+    enriched_neg <- enrichr(limma_rock_neg, terms); enriched_neg <- do.call(rbind, enriched_neg); enriched_neg$sign <- "negative"
+    enriched <- rbind(enriched_pos, enriched_neg)
+  }
+  #
 }
-#
